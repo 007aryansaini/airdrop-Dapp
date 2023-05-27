2eb074d40c7d71c2490c17a9ce9aa6734d6bcc23
aryan <aryansaini0007@gmail.com>
2023-05-27 15:36:08 +0530

    Different BNB airdrop

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SendAirdrop from "./components/SendAirdrop/SendAirdrop";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import AirdropTypes from "./components/AirdropTypes/AirdropTypes";
+import AirdropBNBDifferent from "./components/AirdropBNBDifferent/AirdropBNBDifferent";
 
 const App = () => {
   const [state, setState] = useState({
@@ -16,9 +18,14 @@ const App = () => {
   return (
     <Router>
       <Header saveState={saveState} />
+      <AirdropTypes />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/simple-airdrop" element={<SendAirdrop state={state} />} />
+        <Route
+          path="/different-BNB-airdrop"
+          element={<AirdropBNBDifferent state={state} />}
+        />
       </Routes>
     </Router>
   );
---
+++ client/src/components/AirdropTypes/AirdropTypes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./AirdropTypes.css";
+import { Link } from "react-router-dom";
+
+const AirdropTypes = () => {
+  return (
+    <div className="airdrop-types-container">
+      <Link to={"/simple-airdrop"} className="types-heading">
+        Airdrop equal BNB
+      </Link>
+      <Link to={"/different-BNB-airdrop"} className="types-heading">
+        Airdrop different BNB
+      </Link>
+      <Link to={"/simple-airdrop"} className="types-heading">
+        Airdrop equal Tokens
+      </Link>
+      <Link to={"/simple-airdrop"} className="types-heading">
+        Airdrop Different Tokens
+      </Link>
+    </div>
+  );
+};
+
+export default AirdropTypes;
