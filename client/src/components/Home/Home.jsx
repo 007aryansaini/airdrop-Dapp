ccf0c3981ce28d7902b324b160447ca2380b3027
aryan <aryansaini0007@gmail.com>
2023-05-29 11:21:38 +0530

    Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home-container">Home</div>;
+  return (
+    <div className="home-container">
+      <img
+        src="https://i.postimg.cc/P56MqBkf/home.jpg"
+        alt="Best Crypto Airdrop Dapp"
+      />
+    </div>
+  );
 };
 
 export default Home;
